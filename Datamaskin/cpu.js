2077d149808b7c63adee9a81618d7655efdfb3bc
Thomas Gloppen <thgloppen@gmail.com>
2019-10-02 09:07:21 +0200

    popup box

---
+++ Datamaskin/cpu.js
@@ -0,0 +1,17 @@
+// @ts-check
+
+function setup() {
+    let skisse = document.getElementById("skisse");
+    skisse.addEventListener("click", showme);
+
+    function showme(e) {
+        let u = e.target;
+        if (u.dataset && u.dataset.more) {
+            location.href = u.dataset.more + ".html";
+        }
+
+    }
+
+    start();
+
+}
